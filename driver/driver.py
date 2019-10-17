0471767beb12cb4136c6e0beecaf8ba925512aa4
Alberto Maiocco <apmaiocco@gmail.com>
2019-10-17 13:46:53 -0500

    started driver creation, got test file for driver and started processing [am]

---
+++ driver/driver.py
@@ -0,0 +1,10 @@
+infile = open('testText.txt', 'r');
+textArray = infile.readlines();
+infile.close();
+wordArray = [];
+
+print(textArray);
+for line in textArray:
+    wordArray.appendsplit(" ");
+
+textArray;
