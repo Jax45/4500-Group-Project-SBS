ce6d92382f0dbe396967692766eca9378ce589f2
Alberto Maiocco <apmaiocco@gmail.com>
2019-10-22 11:56:33 -0500

    got text filtering working in driver [am]

--- driver/driver.py
+++ driver/driver.py
@@ -1,10 +1,15 @@
-infile = open('testText.txt', 'r');
-textArray = infile.readlines();
-infile.close();
+# needed for filtering
+import string;
+
+#initializing list to hold text
 wordArray = [];
 
-print(textArray);
-for line in textArray:
-    wordArray.appendsplit(" ");
+# open file, and set wordArray as list of words in file without newlines
+with open('testText.txt') as f:
+    wordArray = [word for line in f for word in line.split()]
 
-textArray;
+# filter out punctuation and unrecognizable characters, make lower case
+for i in range(len(wordArray)):
+    wordArray[i] = wordArray[i].translate(str.maketrans('', '', string.punctuation))
+    wordArray[i] = ''.join(filter(lambda x: x in string.printable, wordArray[i]))
+    wordArray[i] = wordArray[i].lower();
