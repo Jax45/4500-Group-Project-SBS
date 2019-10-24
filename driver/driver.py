838e856bd030affbb9c6db9d88e4976b06ca79dc
Alberto Maiocco <apmaiocco@gmail.com>
2019-10-24 13:19:41 -0500

    Counting words with spaces working [am]

--- driver/driver.py
+++ driver/driver.py
@@ -20,6 +20,7 @@ res = list(map(int, temp))
 # set up as list of lists: first list is range of distance between words,
 # second list is the words themselves
 codeSpacePhrase = [res, [codeTemp[0], codeTemp[2]]];
+print(codeSpacePhrase);
 
 #initializing list to hold text
 wordArray = [];
